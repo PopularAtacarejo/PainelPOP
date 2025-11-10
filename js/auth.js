9bdeec43b2bf235ffc2bc365672b7448e2e1c41c
PopularAtacarejo <popularatacarejomkt@gmail.com>
2025-11-10 14:39:07 -0300

    Delete js directory

--- js/auth.js
+++
@@ -1,5 +0,0 @@
-// Gerenciamento de Autenticação (se necessário no futuro)
-const Auth = {
-    // Funções de autenticação podem ser adicionadas aqui
-    // Por enquanto, usamos token fixo no utils.js
-};
